b09946b608b72d99cf45f435506a0b9d2b3604af
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-16 01:12:11 +0200

    Route params

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3])});
+    res.send([1, 2, 3])
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
